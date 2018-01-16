040e5be06714de18c11247f12c7762679ca838f5
Roman Schmid <bummzack@gmail.com>
2018-01-16 17:23:48 +0100

    Started rewriting the test for SS4.

--- tests/FocusPointTest.php
+++ tests/FocusPointTest.php
@@ -2,6 +2,10 @@
 
 namespace Jonom\FocusPoint\Tests;
 
+use SilverStripe\Assets\File;
+use SilverStripe\Assets\Folder;
+use SilverStripe\Assets\InterventionBackend;
+use SilverStripe\Assets\Tests\Storage\AssetStoreTest\TestAssetStore;
 use SilverStripe\Dev\SapphireTest;
 use SilverStripe\Assets\Image;
 use SilverStripe\ORM\DataObject;
@@ -12,78 +16,32 @@ class FocusPointTest extends SapphireTest
 {
     protected static $fixture_file = 'FocusPointTest.yml';
 
-    /**
-     * Set up images used for tests (Copied from framework/tests/mode/ImageTest.php).
-     */
     public function setUp()
     {
         parent::setUp();
 
-        $this->origBackend = Image::get_backend();
-
-        if ($this->skipTest) {
-            return;
-        }
-
-        if (!file_exists(ASSETS_PATH)) {
-            mkdir(ASSETS_PATH);
-        }
-
-        // Create a test folders for each of the fixture references
-        $folderIDs = $this->allFixtureIDs('Folder');
-
-        foreach ($folderIDs as $folderID) {
-            $folder = DataObject::get_by_id('Folder', $folderID);
-
-            if (!file_exists(BASE_PATH."/$folder->Filename")) {
-                mkdir(BASE_PATH."/$folder->Filename");
-            }
-        }
+        // Set backend root to /ImageTest
+        TestAssetStore::activate('ImageTest');
 
         // Copy test images for each of the fixture references
-        $imageIDs = $this->allFixtureIDs('Image');
-        foreach ($imageIDs as $imageID) {
-            $image = DataObject::get_by_id('Image', $imageID);
-            $filePath = BASE_PATH."/$image->Filename";
-            $sourcePath = str_replace('assets/ImageTest/', FOCUSPOINT_DIR.'/tests/', $filePath);
-            if (!file_exists($filePath)) {
-                if (!copy($sourcePath, $filePath)) {
-                    user_error('Failed to copy test images', E_USER_ERROR);
-                }
-            }
+        /** @var File $image */
+        $files = File::get()->exclude('ClassName', Folder::class);
+        foreach ($files as $image) {
+            $sourcePath = __DIR__ . '/images/' . $image->Name;
+            $image->setFromLocalFile($sourcePath, $image->Filename);
         }
+
+        // Set default config
+        InterventionBackend::config()->set('error_cache_ttl', [
+            InterventionBackend::FAILED_INVALID => 0,
+            InterventionBackend::FAILED_MISSING => '5,10',
+            InterventionBackend::FAILED_UNKNOWN => 300,
+        ]);
     }
 
-    /**
-     * Remove images used for tests (Copied from framework/tests/mode/ImageTest.php).
-     */
     public function tearDown()
     {
-        if ($this->origBackend) {
-            Image::set_backend($this->origBackend);
-        }
-
-        // Remove the test files that we've created
-        $fileIDs = $this->allFixtureIDs('Image');
-        foreach ($fileIDs as $fileID) {
-            $file = DataObject::get_by_id('Image', $fileID);
-            if ($file && file_exists(BASE_PATH."/$file->Filename")) {
-                unlink(BASE_PATH."/$file->Filename");
-            }
-        }
-
-        // Remove the test folders that we've created
-        $folderIDs = $this->allFixtureIDs('Folder');
-        foreach ($folderIDs as $folderID) {
-            $folder = DataObject::get_by_id('Folder', $folderID);
-            if ($folder && file_exists(BASE_PATH."/$folder->Filename")) {
-                Filesystem::removeFolder(BASE_PATH."/$folder->Filename");
-            }
-            if ($folder && file_exists(BASE_PATH.'/'.$folder->Filename.'_resampled')) {
-                Filesystem::removeFolder(BASE_PATH.'/'.$folder->Filename.'_resampled');
-            }
-        }
-
+        TestAssetStore::reset();
         parent::tearDown();
     }
 
@@ -92,25 +50,25 @@ class FocusPointTest extends SapphireTest
      */
     public function Images()
     {
-        $pngLeftTop = $this->objFromFixture('Image', 'pngLeftTop');
+        $pngLeftTop = $this->objFromFixture(Image::class, 'pngLeftTop');
         $pngLeftTop->VerticalSliceTopLeftColor = 'ff0000';
         $pngLeftTop->VerticalSliceBottomRightColor = '00ff00';
         $pngLeftTop->HorizontalSliceTopLeftColor = 'ff0000';
         $pngLeftTop->HorizontalSliceBottomRightColor = 'ffff00';
 
-        $pngRightTop = $this->objFromFixture('Image', 'pngRightTop');
+        $pngRightTop = $this->objFromFixture(Image::class, 'pngRightTop');
         $pngRightTop->VerticalSliceTopLeftColor = 'ffff00';
         $pngRightTop->VerticalSliceBottomRightColor = '0000ff';
         $pngRightTop->HorizontalSliceTopLeftColor = 'ff0000';
         $pngRightTop->HorizontalSliceBottomRightColor = 'ffff00';
 
-        $pngRightBottom = $this->objFromFixture('Image', 'pngRightBottom');
+        $pngRightBottom = $this->objFromFixture(Image::class, 'pngRightBottom');
         $pngRightBottom->VerticalSliceTopLeftColor = 'ffff00';
         $pngRightBottom->VerticalSliceBottomRightColor = '0000ff';
         $pngRightBottom->HorizontalSliceTopLeftColor = '00ff00';
         $pngRightBottom->HorizontalSliceBottomRightColor = '0000ff';
 
-        $pngLeftBottom = $this->objFromFixture('Image', 'pngLeftBottom');
+        $pngLeftBottom = $this->objFromFixture(Image::class, 'pngLeftBottom');
         $pngLeftBottom->VerticalSliceTopLeftColor = 'ff0000';
         $pngLeftBottom->VerticalSliceBottomRightColor = '00ff00';
         $pngLeftBottom->HorizontalSliceTopLeftColor = '00ff00';
@@ -143,8 +101,6 @@ class FocusPointTest extends SapphireTest
             $this->assertEquals($img->HorizontalSliceTopLeftColor, $leftTopColor);
             $this->assertEquals($img->HorizontalSliceBottomRightColor, $bottomRightColor);
 
-            // Clear cache since we're changing the focus point
-            $img->deleteFormattedImages();
         }
     }
 
@@ -193,8 +149,6 @@ class FocusPointTest extends SapphireTest
             $this->assertEquals($img->HorizontalSliceTopLeftColor, $leftTopColor);
             $this->assertEquals($img->HorizontalSliceBottomRightColor, $bottomRightColor);
 
-            // Clear cache since we're changing the focus point
-            $img->deleteFormattedImages();
         }
     }
 }
